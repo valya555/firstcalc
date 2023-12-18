8245cd021e0520da0b2a4e57372edd67100174d6
OmegaCollegeUser123 <OmegaCollegeUser123@gmail.com>
2023-12-18 11:13:06 +0200

    first functions

---
+++ main.py
@@ -0,0 +1,12 @@
+
+def returnAdd(num1,num2):
+    return num1 + num2
+def returnSub(num1,num2):
+    return num1 - num2
+def returnMul(num1,num2):
+    return num1 * num2
+def returnDiv(num1,num2):
+    return num1 / num2
+def returnPow(num1,num2):
+    return num1 ** num2
+
